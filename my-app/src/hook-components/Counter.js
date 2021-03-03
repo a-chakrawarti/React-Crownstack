5fff8ba5348107ff00e94b58c8f8ff777c99ad46
CURVX <chakrawarti.anupam@gmail.com>
2021-03-03 14:28:12 +0530

    Styling changes and prettier setup

--- my-app/src/hook-components/Counter.js
+++ my-app/src/hook-components/Counter.js
@@ -1,28 +1,47 @@
-import React, { useState } from 'react';
-import '../HookApp.css'
+import React, { useState } from "react";
+import "../HookApp.css";
 
 function Counter() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+  const btnStyle = "button";
 
-    const initialCount = 0
-    const [count, setCount] = useState(initialCount)
-    const btnStyle = 'button'
-
-    function incrementFive() {
-        for (let i=0; i<5; i++) {
-            setCount(prevCount => prevCount + 1)
-        }
+  function incrementFive() {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
     }
+  }
 
-    return (
-        <div>
-            <h2>Count: {count}</h2>
-            <button className = {btnStyle} onClick={() => setCount(count + 1)}>Unsafe Increment</button> {/* Unsafe */}
-            <button className = {btnStyle} onClick={() => setCount(initialCount)}>Reset</button> {/* Accessing the state using a prevState is safe */}
-            <button className = {btnStyle} onClick={() => setCount(prevCount => prevCount + 1)}>Increment</button> {/* Accessing the state using a prevState is safe */}
-            <button className = {btnStyle} onClick={() => setCount(prevCount => prevCount - 1)}>Decrement</button> {/* Accessing the state using a prevState is safe */}
-            <button className = {btnStyle} onClick={incrementFive}>Increment Five</button>
-        </div>
-    )
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+      <button className={btnStyle} onClick={() => setCount(count + 1)}>
+        Unsafe Increment
+      </button>{" "}
+      {/* Unsafe */}
+      <button className={btnStyle} onClick={() => setCount(initialCount)}>
+        Reset
+      </button>{" "}
+      {/* Accessing the state using a prevState is safe */}
+      <button
+        className={btnStyle}
+        onClick={() => setCount((prevCount) => prevCount + 1)}
+      >
+        Increment
+      </button>{" "}
+      {/* Accessing the state using a prevState is safe */}
+      <button
+        className={btnStyle}
+        onClick={() => setCount((prevCount) => prevCount - 1)}
+      >
+        Decrement
+      </button>{" "}
+      {/* Accessing the state using a prevState is safe */}
+      <button className={btnStyle} onClick={incrementFive}>
+        Increment Five
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
--- my-app/src/hook-components/InputName.js
+++ my-app/src/hook-components/InputName.js
@@ -1,26 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../HookApp.css";
 
 const InputName = () => {
+  const [name, setName] = useState({
+    firstName: "",
+    lastName: "",
+  });
 
-    const [name, setName] = useState({
-        firstName: '',
-        lastName: ''
-    })
+  function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
-    return (
-        <div>
-            <input type="text" 
-            value={name.firstName}
-            onChange={event => setName({...name, firstName: event.target.value})}>
-            </input>
-            <input type="text" 
-            value={name.lastName}
-            onChange={event => setName({...name, lastName: event.target.value})}>
-            </input>
+  let welcomeMessage = null;
 
-            <h3>{`Welcome, ${name.firstName} ${name.lastName}`}</h3>
-        </div>
-    );
-}
- 
+  if (name.firstName.length === 0 && name.lastName.length === 0) {
+    welcomeMessage = "guest";
+  } else {
+    welcomeMessage = `${capitalize(name.firstName)} ${capitalize(
+      name.lastName
+    )}`;
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={name.firstName}
+        onChange={(event) =>
+          setName({ ...name, firstName: event.target.value })
+        }
+      ></input>
+      <input
+        type="text"
+        value={name.lastName}
+        onChange={(event) => setName({ ...name, lastName: event.target.value })}
+      ></input>
+      <h3>
+        Welcome<span className="greet"> {welcomeMessage}</span>
+      </h3>
+    </div>
+  );
+};
+
 export default InputName;
